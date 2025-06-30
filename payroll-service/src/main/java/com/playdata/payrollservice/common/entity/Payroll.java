1ad78a36d8484a71c8b9eaa2f59ae5345f6f4393
hajoon <stravinskky@naver.com>
2025-06-30 09:41:10 +0900

    yhj_B 브랜치 푸시

---
+++ payroll-service/src/main/java/com/playdata/payrollservice/common/entity/Payroll.java
@@ -0,0 +1,10 @@
+package com.playdata.payrollservice.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+
