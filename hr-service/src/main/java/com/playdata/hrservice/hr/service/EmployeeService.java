cff0c375f4da39c7b7c440773dea4204dd90af97
ehgus8 <68594605+ehgus8@users.noreply.github.com>
2025-07-29 13:18:31 +0900

    어드민 정보 수정할수있게복구

--- hr-service/src/main/java/com/playdata/hrservice/hr/service/EmployeeService.java
+++ hr-service/src/main/java/com/playdata/hrservice/hr/service/EmployeeService.java
@@ -237,9 +237,9 @@ public class EmployeeService {
         Employee employee = employeeRepository.findById(id).orElseThrow(
                 () -> new EntityNotFoundException("Employee not found!")
         );
-        if (employee.getRole().equals(Role.ADMIN)) {
-            throw new BadRequestException("어드민 정보는 수정할 수 없습니다!");
-        }
+//        if (employee.getRole().equals(Role.ADMIN)) {
+//            throw new BadRequestException("어드민 정보는 수정할 수 없습니다!");
+//        }
         if (role.equals(Role.ADMIN) || role.equals(Role.HR_MANAGER)) {
             employee.updateRoleAndPosition(Role.valueOf(dto.getRole()), Position.valueOf(dto.getPosition()));
             employee.updateDepartment(departmentService.getDepartmentEntity(dto.getDepartmentId()));
